5263040ca6f2ecd9e2bef57f7708645f5dec399c
84436 <thien.bui.84436@gmail.com>
2020-08-16 02:17:28 +0700

    [*] map: noghost, someghost; main: cosmetic fix đôi chỗ

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@
 import os
 import sys
 
+# Supress PyGame welcome message
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 # External: PyGame (for drawing)
 import pygame
 pgload = pygame.image.load
@@ -22,6 +24,7 @@ from pacman.level_4 import *
 
 # Constants
 PG_OBJECT_SIZE = 24
+STEP_DELAY = 250 # ms
 
 # Objects
 WALL = 1
@@ -35,7 +38,7 @@ ASSETS_DIR = BASE_DIR + '/assets'
 MAPS_DIR   = BASE_DIR + '/maps'
 
 # Set maps and levels (from default or from arguments)
-MAP = '01_19-21_macpan.txt'
+MAP = '01_19-21_macpan'
 LEVEL = 1
 try:
     if len(sys.argv) == 1:
@@ -50,9 +53,9 @@ try:
         if LEVEL not in range(1, 2+1): raise
         # Does this map exists?
         MAP = sys.argv[2]
-        if not os.path.exists(MAPS_DIR + '/' + MAP): raise
+        if not os.path.exists(MAPS_DIR + '/' + MAP + '.txt'): raise
 except:
-    print('ERROR: Invalid arguments/Map does not exist/Invalid level number.\nPass at most 2 arguments: [level 1-2] [mapfile.txt]. Place mapfile in ./maps folder')
+    print('ERROR: Invalid arguments/Map does not exist/Invalid level number.\nPass at most 2 arguments: [level 1-2] [mapfile (no extension)]. Place mapfile.txt in ./maps folder')
     exit(-1)
 
 ################################################################################
@@ -60,13 +63,12 @@ except:
 
 # Parse map
 the_map = map_utils.Map()
-with open(MAPS_DIR + '/' + MAP, 'r') as mapfile:
+with open(MAPS_DIR + '/' + MAP + '.txt', 'r') as mapfile:
     the_map.parse_file(mapfile)
 
 # Create level solver
 level_picker = [None, Level_1_2, Level_1_2, Level_3, Level_4]
 level = level_picker[LEVEL](the_map)
-print(type(level))
 
 # PyGame: prepare images and parameters
 objects = {
@@ -86,8 +88,6 @@ w_loop = True
 screen = w.set_mode(window_size)
 surface_walls = pygame.Surface(flags=pygame.SRCALPHA, size=window_size)
 surface_food = pygame.Surface(flags=pygame.SRCALPHA, size=window_size)
-
-################################################################################
 
 # Map: draw all (blind)
 def pg_draw_all_blind():
@@ -128,13 +128,17 @@ def pg_draw_base(draw_mode=1):
     pg_draw_food()
     screen.blit(surface_food, (0, 0))
 
-# level_1 = level.Level_1_2(the_map)
-# level_1.run(steps=0)
-# while (level_1._game_state == 0):
-#    level_1.run(steps=1)
+################################################################################
+# MAIN LOOP
+
+# Print basic info
+print('Level = {l}\nMap   = {m}\nCtrl-C here to stop.'.format(
+    l = LEVEL,
+    m = MAP
+))
 
 STEP_ALL = True
-STEP_DELAY = 250 # ms
+GAME_OVER_PRINT_MSG = False
 
 pg_draw_all_blind()
 while w_loop:
@@ -159,6 +163,9 @@ while w_loop:
         pygame.time.delay(STEP_DELAY)
 
     # Event queue: game over
+    if not GAME_OVER_PRINT_MSG:
+        print('Game over.')
+        GAME_OVER_PRINT_MSG = True
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             w_loop = False
